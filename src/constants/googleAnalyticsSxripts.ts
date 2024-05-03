33f32b188f554d78d38e975151e5987cec08b119
Richard Rusnak <richard.rusnak97@gmail.com>
2024-05-03 12:44:09 +0200

    Update GA

--- src/constants/googleAnalyticsSxripts.ts
+++ src/constants/googleAnalyticsSxripts.ts
@@ -1,13 +1,13 @@
 export const GA_TRACKING_SCRIPT = `
-    window.dataLayer = window.dataLayer || [];
-    function gtag(){dataLayer.push(arguments);}
-    gtag('js', new Date());
+window.dataLayer = window.dataLayer || [];
+function gtag(){dataLayer.push(arguments);}
+gtag('js', new Date());
 
-    gtag('config', '${process.env.NEXT_GA_TRACKING}');
+gtag('config', 'G-FMD5QP7V33');
 `;
 
-export const GTM_SCRIPT = `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+export const GTM_SCRIPT = `<script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
 new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
 j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
 'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-})(window,document,'script','dataLayer','${process.env.NEXT_PUBLIC_GTM}');`;
+})(window,document,'script','dataLayer','${process.env.NEXT_PUBLIC_GTM}');</script>`;
